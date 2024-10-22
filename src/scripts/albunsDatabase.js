0e885cd3a830dd177aac7df91973d083110208e9
Nicolas Voigt <144836055+Mefanick@users.noreply.github.com>
2024-10-22 09:42:01 -0300

    "Tarefa 3 concluída"

--- src/scripts/albunsDatabase.js
+++ src/scripts/albunsDatabase.js
@@ -1,51 +1,49 @@
-export default albumList;
-
 const albumList = [
     {
       title: "Acabou o Chorare",
       genre: "MPB",
       band: "Novos Baianos",
-      price: "137.91",
+      price: "137,91",
       img: "./src/assets/imgs/cover-1.png",
     },
     {
       title: "The Wall",
       genre: "Rock",
       band: "Pink Floyd",
-      price: "123.56",
+      price: "123,56",
       img: "./src/assets/imgs/cover-2.png",
     },
     {
       title: "To Pimp a Butterfly",
       genre: "Rap",
       band: "Kendrick Lamar",
-      price: "102.66",
+      price: "102,66",
       img: "./src/assets/imgs/cover-3.png",
     },
     {
       title: "Thriller",
       genre: "Pop",
       band: "Michael Jackson",
-      price: "142.32",
+      price: "142,32",
       img: "./src/assets/imgs/cover-4.png",
     },
     {
       title: "All Eyez on Me",
       genre: "Hip-Hop",
       band: "2Pac",
-      price: "50.13",
-      img: "./src/assets/imgs/cover-5.png",
+      price: "50,13",
+      img: "./src/assets/imgs/cover-5d.png",
     },
     {
       title: "Dom de Sambar",
       genre: "Samba",
       band: "Turma do Pagode",
-      price: "51.67",
+      price: "51,67",
       img: "./src/assets/imgs/cover-6.png",
     },
   ];
 
-
+  export default albumList;
   /*
     criar albuns dinamicos, (funcao que cria album e tira com base no database)
     revisar conteudos antigos
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,4 @@
-import { albumList } from "./albunsDatabase"
+import  albumList  from "./albunsDatabase.js"
 
 function applyInputRangeStyle(){
     const inputRange = document.querySelector("#rangeInput")
@@ -12,9 +12,63 @@ function applyInputRangeStyle(){
     });
 }
 
+function createAlbum(index) {
+    const ul = document.querySelector("#albunsContainer");
+    const li = document.createElement("li");
+    const img = document.createElement("img");
+    const div = document.createElement("div");
+    const titleDiv = document.createElement("div");
+    const genereDiv = document.createElement("div");
+    const priceDiv = document.createElement("div");
+    const h3 = document.createElement("h3");
+    const p1 = document.createElement("p");
+    const p2 = document.createElement("p");
+    const h4 = document.createElement("h4");
+    const button = document.createElement("button");
+
+    ul.appendChild(li);
+    li.appendChild(img);
+    li.appendChild(div)
+    div.appendChild(titleDiv);
+    div.appendChild(genereDiv);
+    div.appendChild(priceDiv);
+    titleDiv.appendChild(h3);
+    genereDiv.appendChild(p1);
+    genereDiv.appendChild(p2);
+    priceDiv.appendChild(h4);
+    priceDiv.appendChild(button);
+
+    li.classList.add("album__box");
+
+    img.src = albumList[index].img;
+    div.classList.add("album__comment--box");
+
+    titleDiv.classList.add("album__comment--title");
+    h3.innerText = albumList[index].title;
+
+    genereDiv.classList.add("album__comment--genre");
+    p1.innerText = albumList[index].band;
+    p2.innerText = albumList[index].genre;
+
+    priceDiv.classList.add("album__comment--price");
+    button.classList.add("album__button")
+    h4.innerText = "R$" + albumList[index].price;
+    button.innerText = "Comprar";
+}
+function renderElements(albuns) {
+    const ul = document.querySelector("#albunsContainer");
+    for (let i = 1; i <= albuns.length; i++) {
+        let li = document.querySelector('.album__box');
+        li.remove();
+    }
+    for (let i = 0;i < albuns.length; i++) {
+        createAlbum(i);
+    }
+    
+}
 
 function routine(){
     applyInputRangeStyle();
-    console.log(albumList[1])
+    renderElements(albumList);
 }
 routine();
